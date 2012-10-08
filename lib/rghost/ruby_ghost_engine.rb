eeb1d8eeb87f60a6ec213c49d6e399a46277be1e
Shairon Toledo <shairon.toledo@gmail.com>
2012-10-08 11:36:08 -0300

    Fixing page blank https://github.com/shairontoledo/rghost/issues/19

--- lib/rghost/ruby_ghost_engine.rb
+++ lib/rghost/ruby_ghost_engine.rb
@@ -39,7 +39,7 @@ class RGhost::Engine
     params << @document.additional_params.join(" ") unless @options[:convert]
     params << "-I#{RGhost::Config::GS[:pslibdir]}"
     params << "-dPDFSETTINGS=/#{@options[:quality]}" if (@options[:device] ==:pdf && @options[:quality])
-    params << "-I#{RGhost::Config::GS[:extensions].join(' -I')}" if RGhost::Config::GS[:extensions]
+    params << "-I#{RGhost::Config::GS[:extensions].join(' -I')}" unless RGhost::Config::GS[:extensions].empty?
     params << "-sDEVICE=#{device_for(device)}"
     params.concat format_params(@options[:d],"-d") if  @options[:d]
     params.concat format_params(@options[:s],"-s") if  @options[:s]
