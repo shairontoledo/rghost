5d02c8688552a3d18718d9047dae6446b496a9a5
Steven Harman <steven@harmanly.com>
2024-03-08 22:15:55 -0500

    Remove unnecessary global
    This can just be a constant in the re-opened RGhost module.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,14 +4,15 @@ require File.expand_path("../lib/rghost", __dir__)
 
 RGhost::Config.config_platform
 
-$RG_TMP_DIR=Dir.mktmpdir(Time.now.to_i.to_s)
-
 module RGhost
+  RG_TMP_DIR = Dir.mktmpdir(Time.now.to_i.to_s)
+  private_constant :RG_TMP_DIR
+
   def self.using_temp_dir(filepath)
-    File.expand_path(File.join($RG_TMP_DIR,filepath))
+    File.expand_path(File.join(RG_TMP_DIR, filepath))
   end
 
   def self.asser_path(filename)
-    File.expand_path(File.join(File.dirname(__FILE__),'assets', filename))
+    File.expand_path(File.join(File.dirname(__FILE__), "assets", filename))
   end
 end
