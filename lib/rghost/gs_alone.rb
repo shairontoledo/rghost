608182d2020ec6d9517aae4919d96239e18a01a4
Steven Harman <steven@harmanly.com>
2024-03-09 16:12:17 -0500

    Document the param overwriting
    This confused the heck of me until I tracked down where params were
    coming from. I think we could/should probably just remove that from the
    default params and instead add it in when needed. But for now, just a
    comment.

--- lib/rghost/gs_alone.rb
+++ lib/rghost/gs_alone.rb
@@ -1,20 +1,17 @@
 class RGhost::GSAlone # :nodoc:
   def initialize(params, debug = false)
     @params = params.dup
-    @params[0] = " "
+    @params[0] = " " # Get rid of the "gs" command from RGhost::Config::GS[:default_params]
     @debug = debug
   end
 
   def run
     cmd = @params.join(" ")
-    # puts File.exist?(RGhost::Config::GS[:path].to_s)
     unless File.exist?(RGhost::Config::GS[:path].to_s)
       RGhost::Config.config_platform
     end
     r = system(RGhost::Config::GS[:path] + cmd)
-
     puts RGhost::Config::GS[:path] + cmd if @debug
-    # puts r
     r
   end
 end
